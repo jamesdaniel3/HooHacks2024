5dcfcf2df6eeff4c18412ab9feaf15fc92a47854
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-03-23 14:34:16 -0400

    initial model for jars

--- the_jar/models.py
+++ the_jar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Jar(models.Model):
+    class ModeChoices(models.TextChoices):
+        OPTION_ONE = 'Option One', 'Option One Description'
+        OPTION_TWO = 'Option Two', 'Option Two Description'
+        OPTION_THREE = 'Option Three', 'Option Three Description'
+
+    name = models.CharField(max_length=100)
+    users_in_jar = models.ManyToManyField(User)
+    mode = models.CharField(max_length=20, choices=ModeChoices.choices)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    reward = models.CharField(max_length=100, null=True, blank=True)
+    punishment = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
