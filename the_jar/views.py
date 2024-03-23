157ddb812c7f77f2520748dbeef82762d16378f6
caeyo <oppermancarter@gmail.com>
2024-03-23 18:31:44 -0400

    Add ability to create basic jar

--- the_jar/views.py
+++ the_jar/views.py
@@ -1,5 +1,8 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, get_user_model
+
+from the_jar.models import Jar
 
 
 def home(request):
@@ -15,7 +18,30 @@ def user_home(request):
     return render(request, 'the_jar/user_home.html')
 
 def create_jar(request):
-    return
+    class JarForm(forms.ModelForm):
+        class Meta:
+            model = Jar
+            fields = ('name', 'mode', 'start_date', 'end_date', 'reward', 'punishment')
+            widgets = {
+                'end_date': forms.DateInput(attrs={'type': 'date'}),
+                'start_date': forms.DateInput(attrs={'type': 'date'})
+            }
+
+    """
+    on submit:
+    - Need to get the user submitting and add it to the users in the jar as it submits
+
+    need to conditionally display fields based on mode - maybe multi-step form?
+    """
+
+    if request.method == 'POST':
+        form = JarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/user_home')
+    else:
+        form = JarForm()
+    return render(request, "the_jar/create_jar.html", {'form': form})
 
 def view_jar(request):
     return
