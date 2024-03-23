b668acb3f3bdb5d36217f1c25620c47b004e504f
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-03-23 13:46:05 -0400

    implemented google login

--- the_jar/urls.py
+++ the_jar/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home),
+    path("logout", views.logout_view),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- the_jar/views.py
+++ the_jar/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
     return render(request, 'the_jar/home.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+
